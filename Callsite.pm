fe51691db2db0278bafb05dba00d76ebd2475317
Rocky Bernstein <rocky@cpan.org>
2012-09-15 02:07:00 +0000

    Import of ROCKY/Devel-Callsite-0.05 from CPAN.
    gitpan-cpan-distribution: Devel-Callsite
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         ROCKY/Devel-Callsite-0.05.tar.gz
    gitpan-cpan-author:       ROCKY
    gitpan-cpan-maturity:     released

--- Callsite.pm
+++ Callsite.pm
@@ -3,45 +3,74 @@ use 5.005;
 use vars qw($VERSION);
 require DynaLoader;
 use base qw(DynaLoader Exporter);
-$VERSION = '0.04';
+$VERSION = '0.05';
 @EXPORT = qw/callsite context/;
 
 bootstrap Devel::Callsite;
 
+# Demo code
+unless (caller) { 
+  my $site = sub { return callsite() };
+  printf "OP location: 0x%x\n", $site->(); # prints caller OP location
+  printf "OP location: 0x%x\n", $site->(); # prints a different OP location
+
+  print context(), "\n"; # prints the interpreter context, a number
+}
+
 1;
 __END__
+=for comment
+This file is shared by both README.pod and Callsite.pm after its __END__
+It is also in the github wiki:
+https://github.com/rocky/Perl-Devel-Callsite/wiki
+where we can immediately see the results and others can contribute.
 
+=for comment
 =head1 NAME
 
-Devel::Callsite - Get current callsite and interpreter context
+Devel::Callsite - Get caller return OP address and Perl interpreter context
 
 =head1 SYNOPSIS
 
   use Devel::Callsite;
-  sub $site { return callsite() };
-  print $site->(), "\n"; # prints one number
-  print $site->(), "\n"; # prints a different number
+  my $site = sub { return callsite() };
+  printf "OP location: 0x%x\n", $site->(); # prints caller OP location
+  printf "OP location: 0x%x\n", $site->(); # prints a different OP location
 
   print context(), "\n"; # prints the interpreter context, a number
 
 =head1 DESCRIPTION
 
-The callsite() function returns the callsite (a number) one level up
-from where it was called.  See the tests for an example.  It's useful
-for functions that need to uniquely know where they were called, such
-as Every::every() (see CPAN for that module).
+The I<callsite()> function returns the the OP address of the caller, a
+number, one level up from where it was called. It's useful for
+functions that need to uniquely know where they were called, such as
+I<Every::every()>; see L<Every>. Or it can be used to L<pinpoint a
+location with finer granularity than a line
+number|http://www.perlmonks.com/?node_id=987268>. In conjunction with
+an OP tree disassembly you can know exactly where the caller is
+located in the Perl source.
 
-The context() function returns the interpreter context as a number.
+The I<context()> function returns the interpreter context as a number.
 This is a fairly unique number together with the call site.
 
 =head1 HISTORY
 
-Written by Ben Morrow on perl5-porters.  CPAN-ified by Ted Zlatanov.
+Ben Morrow conceived this and posted it to perl5-porters. Ted Zlatanov
+then turned it into a CPAN module which he maintained for the first 3
+revisions.
 
-=head1 AUTHOR
+It is currently maintained (or not) by Rocky Bernstein.
 
-Ben Morrow <ben@morrow.me.uk>
+=head1 SEE ALSO
+
+L<B::Concise> to disassemble the OP tree. L<Devel::Trepan> optionally
+uses I<Devel::Callsite> to show you exactly where you are stopped
+inside the debugger.
+
+=head1 AUTHORS
+
+Rocky Bernstein <rocky@cpan.org> (current maintainer)
 Ted Zlatanov <tzz@lifelogs.com>
+Ben Morrow
 
 =cut
-
